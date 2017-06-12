584c891d3a8ab98d13ae37d7fe8892fdea0df138
Nigel-J <nijaffe@packer.edu>
2017-06-12 15:11:50 -0400

    Create TSL2561_phototaxis.py

--- TSL2561_phototaxis.py
+++ TSL2561_phototaxis.py
@@ -7,19 +7,60 @@ import math
 import initTSL2561
 
 LEFT_TRIM=0
-RIGHT_TRIM=5
+RIGHT_TRIM=0
 
 robot=Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
 thistime = 0
 lasttime = 0
 
+#begin with short run
+
 while True:
-        thistime=LightSensor.calculateLux()
-        print thistime - lasttime, "Lux"
-
         robot.backward(150,0.2)
-
+        
+        #headsweep(s)
+        t = random.random()
+        if t>0.5:
+                #sweep left
+                robot.left(172, 0.25)
+                #take reading
+                thistime = LightSensor.calculateLux()
+                #probability of accepting
+                delta = (thistime-lasttime)
+                print delta "Lux;" + '\n'
+                pa = 1/(1+math.exp(-1*(-1*(float(delta)/5)-0.85)))
+                # Print probablility of accepting? print pa  + '\n'
+                w = random.random()
+                if w<pa:
+                        #Accepted left head sweep
+                        robot.backward(150,0.2)
+                        print "Accepted;" + '\n'
+                        
+                else: 
+                        print "Rejected;" + '\n'
+                        robot.right(172, 0.25)
+                         
+        else:
+                #sweep right
+                robot.right(172, 0.25)
+                #take reading
+                thistime = LightSensor.calculateLux()
+                #probability of accepting
+                delta = (thistime-lasttime)
+                print delta "Lux;" + '\n'
+                pa = 1/(1+math.exp(-1*(-1*(float(delta)/5)-0.85)))
+                # Print probablility of accepting? print pa  + '\n'
+                w = random.random()
+                if w<pa:
+                        #Accepted right head sweep
+                        robot.backward(150,0.2)
+                        print "Accepted;" + '\n'
+                else: 
+                        print "Rejected;" + '\n'
+                        robot.left(172, 0.25)
+                
+        ###
         delta=(thistime-lasttime)
         pt=1/(1+math.exp(-1*(-1*(float(delta)/5)-0.5)))
         print pt
