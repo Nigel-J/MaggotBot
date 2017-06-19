567544a9b6bf06973e99b659add32b7b09b2717a
Nigel-J <nijaffe@packer.edu>
2017-06-19 12:35:37 -0400

    Update Right_Sweep.py

--- Right_Sweep.py
+++ Right_Sweep.py
@@ -9,10 +9,13 @@ print delta "Lux;" + '\n'
 pa = 1/(1+math.exp(-1*(float(delta)/5)-0.85)))
 # Print probablility of accepting? print pa  + '\n'
 w = random.random()
+
 if w<pa:
-#Accepted right head sweep
-robot.backward(150,0.2)
-print "Accepted;" + '\n'
+  #Accepted right head sweep
+  robot.backward(150,0.2)
+  print "Accepted;" + '\n'
+  
 else: 
-print "Rejected;" + '\n'
-robot.left(172, 0.25)
+  print "Rejected;" + '\n'
+  robot.left(172, 0.25)
+  os.system("Left_Sweep.py")
