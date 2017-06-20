47d4ec702df2fa5f2749a7e70015db5269574114
Nigel-J <nijaffe@packer.edu>
2017-06-20 13:35:37 -0400

    Update Right_Sweep.py

--- Right_Sweep.py
+++ Right_Sweep.py
@@ -7,7 +7,7 @@ thistime = LightSensor.calculateLux()
 delta = (thistime-lasttime)
 print delta + "Lux;" + '\n'
 pa = 1/(1+math.exp(-1*(float(delta)/5)-0.85))
-# Print probablility of accepting? print pa  + '\n'
+# Print probablility of accepting? print pa + '\n'
 w = random.random()
 
 if w<pa:
