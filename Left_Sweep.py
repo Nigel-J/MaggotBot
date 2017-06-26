0ca9a3e1dc6487fc4ba23a51cf8d0516e3fa0957
Nigel-J <nijaffe@packer.edu>
2017-06-26 12:43:24 -0400

    Update Left_Sweep.py

--- Left_Sweep.py
+++ Left_Sweep.py
@@ -11,44 +11,26 @@ RIGHT_TRIM=0
 
 robot=Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
-thistime = 0
-lasttime = 0
+lasttime = LightSensor.calculateLux()
+robot.left(70,0.7)
+#take reading
+thistime = LightSensor.calculateLux()
+#probability of accepting
+delta = (thistime-lasttime)
+print "Delta is", delta, "Lux"
+pa = 1/(1+math.exp(-1*(float(delta)/5)-0.85))
+print("P-%s is %s " % ("accept", pa))
+w = random.random()
 
-while True:
-
-        #begin with short run
+if w<pa:
+        #Accepted left head sweep
         robot.forward(150,0.2)
+        print "Accepted;" + '\n'
 
-        #judge if life is improving
-        thistime = LightSensor.calculateLux()
-        delta=(thistime-lasttime)
-        print "Delta is", delta, "Lux;"
-        pt=1/(1+math.exp(-1*(-1*(float(delta)/5)-0.5)))
-        print "P-turn is", pt
-        r = random.random()
-        
-        if r>pt:
-                #life is good, keep going
-                print "Continue run" + '\n'
-                robot.forward(150,0.2)
-
-        else:
-                #life is not improving, time for head sweeps
-                print "Turn; Begin head sweeps" + '\n'
-
-                lasttime = thistime
-                t = random.random()
-                if t>0.5:
-                        #sweep left
-                        print "Sweep left"
-                        execfile("Left_Sweep.py")
-
-                else:
-                        #sweep right
-                        print "Sweep right"
-                        execfile("Right_Sweep.py")
-                        
+else:
+        print "Rejected; sweep right instead" + '\n'
+        robot.right(70, 0.7)
         lasttime = thistime
-
+        execfile("Right_Sweep.py")
 
   
