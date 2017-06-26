a58102dc90eeeb042bd8ae228fa58b1829ffaaec
Nigel-J <nijaffe@packer.edu>
2017-06-26 12:40:36 -0400

    Update Left_Sweep.py

--- Left_Sweep.py
+++ Left_Sweep.py
@@ -1,23 +1,54 @@
 #!/usr/bin/python
 
-robot.left(172, 0.25)
-#take reading
-thistime = LightSensor.calculateLux()
-#probability of accepting
-delta = (thistime-lasttime)
-print delta "Lux;" + '\n'
-pa = 1/(1+math.exp(-1*(float(delta)/5)-0.85))
-print "P-accept is", pa  + '\n'
-w = random.random()
+import time
+import Robot
+import random
+import math
+from Adafruit_TSL2651 import *
 
-if w<pa:
-  #Accepted left head sweep
-  robot.backward(150,0.2)
-  print "Accepted;" + '\n'
+LEFT_TRIM=0
+RIGHT_TRIM=0
 
-else: 
-  print "Rejected;" + '\n'
-  robot.right(172, 0.25)
-  lasttime = thistime
-  os.system("Right_Sweep")
+robot=Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
+
+thistime = 0
+lasttime = 0
+
+while True:
+
+        #begin with short run
+        robot.forward(150,0.2)
+
+        #judge if life is improving
+        thistime = LightSensor.calculateLux()
+        delta=(thistime-lasttime)
+        print "Delta is", delta, "Lux;"
+        pt=1/(1+math.exp(-1*(-1*(float(delta)/5)-0.5)))
+        print "P-turn is", pt
+        r = random.random()
+        
+        if r>pt:
+                #life is good, keep going
+                print "Continue run" + '\n'
+                robot.forward(150,0.2)
+
+        else:
+                #life is not improving, time for head sweeps
+                print "Turn; Begin head sweeps" + '\n'
+
+                lasttime = thistime
+                t = random.random()
+                if t>0.5:
+                        #sweep left
+                        print "Sweep left"
+                        execfile("Left_Sweep.py")
+
+                else:
+                        #sweep right
+                        print "Sweep right"
+                        execfile("Right_Sweep.py")
+                        
+        lasttime = thistime
+
+
   
