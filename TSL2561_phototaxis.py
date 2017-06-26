c67d0e6c68ed28449a374fd6880dbcbaabe33bda
Nigel-J <nijaffe@packer.edu>
2017-06-26 12:35:41 -0400

    Update TSL2561_phototaxis.py

--- TSL2561_phototaxis.py
+++ TSL2561_phototaxis.py
@@ -2,11 +2,9 @@
 
 import time
 import Robot
-import Left_Sweep
-import Right_Sweep
 import random
 import math
-import initTSL2561
+from Adafruit_TSL2651 import *
 
 LEFT_TRIM=0
 RIGHT_TRIM=0
@@ -17,35 +15,37 @@ thistime = 0
 lasttime = 0
 
 while True:
-        
+
         #begin with short run
-        robot.backward(150,0.2)
+        robot.forward(150,0.2)
         
         #judge if life is improving
         thistime = LightSensor.calculateLux()
         delta=(thistime-lasttime)
+        print "Delta is", delta, "Lux;"
         pt=1/(1+math.exp(-1*(-1*(float(delta)/5)-0.5)))
-        print "P-turn is" + pt + '\n'
-        r = random.random()   
-        
+        print "P-turn is", pt
+        r = random.random()
+
         if r>pt:
                 #life is good, keep going
                 print "Continue run" + '\n'
-                robot.backward(150,0.2)
-                
+                robot.forward(150,0.2)
+
         else:
                 #life is not improving, time for head sweeps
-                print "Turn" + '\n'
-                
+                print "Turn; Begin head sweeps" + '\n'
+
                 lasttime = thistime
                 t = random.random()
                 if t>0.5:
                         #sweep left
-                        os.system("Left_Sweep.py")
-                                
+                        print "Sweep left"
+                        execfile("Left_Sweep.py")
+
                 else:
                         #sweep right
-                        os.system("Right_Sweep.py")
-                
+                        print "Sweep right"
+                        execfile("Right_Sweep.py")
+                        
         lasttime = thistime
-
